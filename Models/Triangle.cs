17f05a88102b7b8baa7d6077b9f3f22b27233840
Grace Lee <gracexsnoopy@gmail.com>
2022-11-09 23:34:06 -0800

    Change namespace name

--- Models/Triangle.cs
+++ Models/Triangle.cs
@@ -1,7 +1,36 @@
-namespace ProjectName
+namespace SideTracker.TriangleTracker
 {
-  public class ClassName
+  public class Triangle
   {
-    // properties, constructors, methods, etc. go here
+    public int SideOne { get; set; }
+    public int SideTwo { get; set; }
+    public int SideThree { get; set; }
+
+    public Triangle(int sideOne, int sideTwo, int sideThree) 
+    {
+      SideOne = sideOne;
+      SideTwo = sideTwo;
+      SideThree = sideThree;
+    }
+    public string CheckType()
+    {
+      if (SideOne > SideTwo + SideThree || SideTwo > SideOne + SideThree || SideThree > SideOne + SideTwo)
+      {
+        return "Not a triangle";
+      }
+      else if (SideOne == SideTwo && SideTwo == SideThree)
+      {
+        return "Equilateral Triangle";
+      }
+      else if (SideOne == SideTwo || SideOne == SideThree || SideTwo == SideThree)
+      {
+        return "Isosceles Triangle";
+      }
+      else 
+      {
+        return "Scalene Triangle";
+      }
+
+    }
   }
 }
