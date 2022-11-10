31264bc3da3490aad7d84bd33569eae442098f99
Grace Lee <gracexsnoopy@gmail.com>
2022-11-09 23:35:08 -0800

    Add UI/console app logic and have user enter three lengths and determine if it makes a triangle

--- Program.cs
+++ Program.cs
@@ -1,3 +1,23 @@
 using System;
-using System.Collections.Generic;
+using SideTracker.TriangleTracker;
 
+namespace SideTracker {
+  public class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine("Enter one number");
+      string length1 = Console.ReadLine();
+      int lengthOne = int.Parse(length1);
+      Console.WriteLine("Enter another number");
+      string length2 = Console.ReadLine();
+      int lengthTwo = int.Parse(length2);
+      Console.WriteLine("Enter one last number");
+      string length3 = Console.ReadLine();
+      int lengthThree = int.Parse(length3);
+
+      Triangle newTriangle = new Triangle(lengthOne, lengthTwo, lengthThree);
+      Console.WriteLine(newTriangle.CheckType());
+    }
+  }
+}
